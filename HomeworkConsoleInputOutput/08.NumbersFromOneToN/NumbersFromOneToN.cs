04860523b24c4e24cc1ba1fe3bd129a1ebd2d7d9
krasi070 <krasi0707@gmail.com>
2015-12-30 17:20:30 +0200

    martin's homework

---
+++ HomeworkConsoleInputOutput/08.NumbersFromOneToN/NumbersFromOneToN.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.NumbersFromOneToN
+{
+    class NumbersFromOneToN
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                Console.WriteLine(i);
+            }
+            
+
+        }
+    }
+}
